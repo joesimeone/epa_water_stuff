334867e01f0eff1fc22cca51479f9b0dc724e8b1
joesimeone <joesimeone9@gmail.com>
2026-05-27 13:35:48 -0400

    Code edits to incorporate Anne Claire's new ucmr data. #6 still needs to be addressed

--- code/weight_contam_flow/03_add_population_weights.R
+++ code/weight_contam_flow/03_add_population_weights.R
@@ -10,43 +10,61 @@ con <- dbConnect(
   read_only = TRUE
 )
 
-dbExecute(con, "LOAD spatial;")
+#dbExecute(con, "LOAD spatial;")
 
 dbListTables(con)
 
+tbl(con, 'weighting.us_tract10_pws_v3') |> filter(st_fips == '11')
 
-PWS_dat <- read.csv(
-  "C:/Users/js5466/OneDrive - Drexel University/Palmer,Amber's files - Preliminary_data_PWScontam/PWS_JOINED_BG_POP.csv"
-)
-
-pws_dat_v1 <-
-  PWS_dat |>
-  select(CT_GEOID, pwsid, starts_with(c('pf', 'chr', 'chl', 'mlyb'))) |>
+pws_dat <-
+  read_csv(here::here(
+    'data',
+    'source_data',
+    'epa_water',
+    'epa_ucmr',
+    'bg10_pwsid_ucmr_dat.csv'
+  )) |>
   mutate(
-    CT_GEOID = as.character(CT_GEOID),
-    geo_tweak = stringr::str_pad(
-      CT_GEOID,
-      width = 11,
-      side = "left",
-      pad = "0"
-    )
+    tract_geoid = substr(GEOID, 1, 11)
   )
 
-glimpse(pws_dat_v1)
+ucmr_combo_dat <-
+  haven::read_sas(
+    'C:/Users/js5466/Drexel University/De Roos,Anneclaire - UCMR_CodedData-PWS_ForMDAC/ucmrcombined_pwscoded.sas7bdat'
+  )
 
-## Water systems with actual contaminant info
+tract_basic <-
+  tbl(con, 'weighting.us_tract10_pws_v3') |>
+  select(GEOID, PWSID) |>
+  collect()
+
+## Water systems with actual contaminant info |
+#' Use this as filter down stream to prevent population weight denominators from being
+#' inflated.
 pwsid_w_contam <-
-  pull(pws_dat_v1, pwsid)
+  ucmr_combo_dat |>
+  semi_join(tract_basic, by = "PWSID") |>
+  distinct(PWSID) |>
+  pull()
 
 ## ========================================================================
 # Derive Population Estimates & Weights -----
 ## ========================================================================
+
+#' Pulling in hawaii & alaska to add population estimates to tract object
+ak_tract_tbl <- tbl(con, 'ak_tract10_3338') |>
+  select(GEOID, tract_total_pop = value) |>
+  collect()
+hi_tract_tbl <- tbl(con, 'hi_tract10_3759') |>
+  select(GEOID, tract_total_pop = value) |>
+  collect()
 
 ## Total Tract Population (Free of any intersections) | Corresponds to Ambers {ct_pop / ct_total_pop}
 ct_pop_joe <-
   tbl(con, 'tract10_5070') |>
   select(GEOID, tract_total_pop = value) |>
   collect() |>
+  rbind(ak_tract_tbl, hi_tract_tbl) |>
   as_tibble() |>
   arrange(GEOID)
 
@@ -63,7 +81,7 @@ ct_pop_joe <-
 ## Inputs used as the weights (I'm just excluding from the actual summary step)
 ## so that I can load them if I need to: call collect() on raw_wgts if need to see / verify
 raw_wgts <-
-  tbl(con, 'block10_group_pws_5070') |>
+  tbl(con, 'weighting.us_bg10_group_pws_v3') |>
   filter(PWSID %in% pwsid_w_contam) |>
   mutate(
     tract_geoid = substr(GEOID, 1, 11),
@@ -81,7 +99,7 @@ raw_wgts <-
 #' Reweighted Population Scaled to tract (Corresponds to Amber's
 #' PWS_dat %>% rename(pop_bg_pws = SUM_POP_BG_PWS))
 pws_reweighted <-
-  tbl(con, 'block10_group_pws_v3_5070') |>
+  tbl(con, 'weighting.us_bg10_group_pws_v3') |>
   filter(PWSID %in% pwsid_w_contam) |>
   mutate(
     tract_geoid = substr(GEOID, 1, 11),
@@ -100,7 +118,7 @@ pws_reweighted <-
     .by = c(tract_geoid, PWSID)
   ) |>
   left_join(
-    tbl(con, 'tract10_pws_v3_5070') |>
+    tbl(con, 'weighting.us_tract10_pws_v3') |>
       select(
         GEOID,
         PWSID,
@@ -116,16 +134,14 @@ pws_reweighted <-
   ) |>
   collect()
 
-pws_reweighted |> filter(is.na(tract10_5070_area))
 
-# ===========================================================================
-#' Population within each tract that intersects a water system
-#' ==========================================================================
+## =======================================================================
+# Population Summaries -----
+## =======================================================================
 #' Using re-weighted population calculated in the last step (pws_reweighted),
 #'     summarise on tract. Corresponds to an estimate of the total tract population served
 #'      by each PWS (?).
 #' Corresponds to Amber's {ct_pop_pws}
-
 pws_tract_pop <-
   pws_reweighted |>
   summarise(
@@ -134,52 +150,64 @@ pws_tract_pop <-
     .by = c(tract_geoid)
   )
 
+#' Corresponds to the total population we estimate is served by a pws
+pws_system_pop <-
+  pws_reweighted |>
+  summarise(
+    n_tracts = n(),
+    pws_total_pop = sum(bg_wgt_pop),
+    .by = c(PWSID)
+  )
 
 ## =======================================================================
 # Joins | Population Estimates, Contaminants, Reweighting -----
 ## =======================================================================
-#' So, now we've got a couple of different population estimates floating across
-#' gegoraphies and intersections. I'm going to pull them all into one place.
-#'
-#' Additionally, we want to relevel our tract populations served by waters system
-#' estimates so that they don't exceed 'true tract population estimates.
-#'   Why: Where blocks were served by multiple systems, their population estimates were
-#'        double counted in the aggregation step. We don't want that.
 
 pws_populations <-
   pws_reweighted |>
   left_join(pws_tract_pop, by = join_by(tract_geoid)) |>
   left_join(ct_pop_joe, by = join_by('tract_geoid' == 'GEOID')) |>
-  # mutate(
-  #   ct_pop_pws_relevel = pmin(ct_pop_pws, tract_total_pop)
-  # ) |>
+  left_join(pws_system_pop, by = join_by('PWSID')) |>
   mutate(
-    pws_ct_total_prop = round(bg_wgt_pop / tract_total_pop, 2), # Proportion of PWS to total CT
-    pws_ct_pwsarea_prop = round(bg_wgt_pop / ct_pop_pws, 2) # Proportion of PWS to CT PWS area
+    pws_ct_total_prop = round(bg_wgt_pop / tract_total_pop, 2),
+    pws_ct_pwsarea_prop = round(bg_wgt_pop / ct_pop_pws, 2)
   )
 
-
-#' Let's add in our contaminants data using an inner_join()?
-#'    Why inner? (I'm sort of guessing | Hard without the actual version of the data)
-#'    - pws_populations represents tracts intersected with water systems &
-#'      rescaled population estimates.
-#'    - pws_dat_v1 represents all systems in the version that
-#'      a) link to a tract
-#'      b) have contaminant data available
-#'    inner_join() returns only the systems that actually have contaminant info available?
+## =======================================================================
+# Tract-Level Weighting -----
+## =======================================================================
+#' Weight contaminants by each PWS's share of the tract population,
+#' then aggregate to tract.
 #'
-#' =====================================================================================
+#'
+chemical_rt_names <- c(
+  'dcpa',
+  'perchlorate',
+  'dichloroethane',
+  'dioxane',
+  'HCFC',
+  'Halon',
+  'pf',
+  'chlorate',
+  'chrom',
+  'cobalt',
+  'molybdenum',
+  'strontium',
+  'vanadium',
+  'fts',
+  'lithium',
+  'haa',
+  'butanol',
+  'germanium',
+  'manganese'
+)
 
-pws_contam <-
+pws_tract_contam <-
   pws_populations |>
-  inner_join(
-    pws_dat_v1,
-    by = join_by(PWSID == pwsid, tract_geoid == geo_tweak)
-  ) |>
-  distinct(tract_geoid, PWSID, .keep_all = TRUE) |>
+  left_join(ucmr_combo_dat, by = join_by(PWSID)) |>
   mutate(
-    wgt_flag = if_else(n_pws > 1, 1, 0), # Will help us determine whether we need to weight contaminants estimates or not
-    population_weight = bg_wgt_pop / ct_pop_pws # Population in each tract water system / total population served by each water system
+    wgt_flag = if_else(n_pws > 1, 1, 0),
+    population_weight = bg_wgt_pop / ct_pop_pws
   ) |>
   group_by(tract_geoid, wgt_flag) |>
   mutate(
@@ -192,50 +220,17 @@ pws_contam <-
   ) |>
   ungroup()
 
-#' Reweight Contaminants | ----
-#'   We have a set of population weights:
-#'      Where a tract is served by only 1 water system ---> those weights are 1
-#'      Where a tract is served by > 1 water systems ---> those weights vary somewhere between 0 and 1
-#'   This step takes each contaminant value and multiplies them by those weights
-#'      Contaminant values will change where a tract is served by multiple water systems,
-#'      Otherwise they'll be the same as the original value (multiplied by 1)
-#' ===========================================================================
-pws_contam <-
-  pws_contam |>
+pws_tract_contam <-
+  pws_tract_contam |>
   mutate(across(
-    starts_with(c('pf', 'chr', 'chl', 'mlyb')),
+    starts_with(chemical_rt_names),
     .fns = ~ .x * population_weight,
     .names = "{.col}_weighted"
-  ))
-
-
-# why_dupes <-
-#   pws_contam |>
-#   filter(wgt_flag == 1) |>
-#   janitor::get_dupes(tract_geoid, PWSID)
-
-# pws_contam |>
-#   filter(wgt_flag == 1) |>
-#   distinct(tract_geoid, PWSID, .keep_all = TRUE) |>
-#   head(5)
-
-# a <-
-#   pws_contam |>
-#   filter(wgt_flag == 0) |>
-#   summarise(min_wgt = min(population_weight), max_wgt = max(population_weight))
-
-# summary(a$max_wgt)
-# summary(a$min_wgt)
-
-## ========================================================================
-# Aggregate to tract  ------
-## ========================================================================
-
-#' So, We have tract - water system contaminants data.
-#' We want to scale this to tract so it can cleanly be assigned to MDAC participants
+  )) |>
+  select(-contains('_detectpws_weighted'))
 
 contam_tract_level <-
-  pws_contam |>
+  pws_tract_contam |>
   group_by(tract_geoid) |>
   summarise(
     across(
@@ -248,12 +243,90 @@ contam_tract_level <-
   left_join(ct_pop_joe, by = join_by(tract_geoid == GEOID)) |>
   left_join(pws_tract_pop, by = join_by(tract_geoid)) |>
   mutate(
-    pws_coverage_proportion = round(ct_pop_pws / tract_total_pop, 3), ## How much of a tract is served by a public water system?
-    included = if_else(pws_coverage_proportion > 0.5, "yes", "no") ## Set coverage threshold at more than 50%
+    pws_coverage_proportion = round(ct_pop_pws / tract_total_pop, 3),
+    included = if_else(pws_coverage_proportion > 0.5, "yes", "no")
   )
 
-janitor::tabyl(contam_tract_level, included)
+## =======================================================================
+# PWS-Level Weighting -----
+## =======================================================================
+#' Weight by each tract's share of the PWS's total served population,
+#' then aggregate to PWS. Note: since contaminant values are constant
+#' per PWS, this recovers the original values. Useful when rolling up
+#' tract-level variables to the system level.
 
-contam_tract_level |>
-  filter(is.na(included)) |>
-  select(tract_geoid, ct_pop_pws, tract_total_pop, included)
+pws_contam <-
+  pws_populations |>
+  left_join(ucmr_combo_dat, by = join_by(PWSID)) |>
+  mutate(
+    population_weight = bg_wgt_pop / pws_total_pop
+  ) |>
+  group_by(PWSID) |>
+  mutate(
+    pop_weight_sum = sum(population_weight, na.rm = TRUE),
+    population_weight = if_else(
+      pop_weight_sum > 1,
+      population_weight / pop_weight_sum,
+      population_weight
+    )
+  ) |>
+  ungroup()
+
+pws_contam <-
+  pws_contam |>
+  mutate(across(
+    starts_with(chemical_rt_names),
+    .fns = ~ .x * population_weight,
+    .names = "{.col}_weighted"
+  )) |>
+  select(-contains('_detectpws_weighted'))
+
+contam_pws_level <-
+  pws_contam |>
+  group_by(PWSID) |>
+  summarise(
+    across(
+      ends_with("_weighted"),
+      ~ round(sum(.x, na.rm = TRUE), 2),
+      .names = "{.col}_sum"
+    ),
+    .groups = "drop"
+  )
+
+
+## =======================================================================
+# write wide data for further uses ----
+## =======================================================================
+
+#' Write local
+write_csv(
+  contam_tract_level,
+  here::here(
+    'data',
+    'results',
+    'weighted_tract_level_ucmr_contam.csv'
+  )
+)
+
+write_csv(
+  contam_pws_level,
+  here::here(
+    'data',
+    'results',
+    'weighted_pws_level_ucmr_contam_wide.csv'
+  )
+)
+
+#' Write to AC One Drive
+#' contam_pws_level
+
+write_csv(
+  contam_pws_level,
+  'C:/Users/js5466/Drexel University/De Roos,Anneclaire - UCMR_CodedData-PWS_ForMDAC/weighted_pws_level_ucmr_contam_wide.csv'
+)
+
+
+write_csv(
+  contam_tract_level,
+  'C:/Users/js5466/Drexel University/De Roos,Anneclaire - UCMR_CodedData-PWS_ForMDAC/weighted_tract_level_ucmr_contam_wide.csv'
+)
